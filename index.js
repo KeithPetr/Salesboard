3000d82ee2ec5d034a5cca0bcf35dc78b54e0a4f
KeithPetr <keith.petryshyn@gmail.com>
2023-02-14 23:14:07 -0500

    Refactored to one function
    Created an addProduct function that takes in the two different products as parameters

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
-const starBtn = document.getElementById('star')
-const fireBtn = document.getElementById('fire')
-const salesIcons = document.getElementById('sales-icons')
-const achievementIcons = document.getElementById('achievement-icons')
-const numberOfSales = document.getElementById('number-sales')
-const numberOfAchievements = document.getElementById('number-achievements')
-const revenueAmount = document.getElementById('revenue-amount')
-const commissionAmount = document.getElementById('commission-amount')
+const starBtn = document.getElementById("star");
+const fireBtn = document.getElementById("fire");
+const salesIcons = document.getElementById("sales-icons");
+const achievementIcons = document.getElementById("achievements-icons");
+const numberOfSales = document.getElementById("number-sales");
+const numberOfAchievements = document.getElementById("number-achievements");
+const revenueAmount = document.getElementById("revenue-amount");
+const commissionAmount = document.getElementById("commission-amount");
+const salesSelectors = document.getElementById("sales-selectors");
+const salesNum = document.getElementById("number-sales");
+const achievementsNum = document.getElementById("number-achievements");
 
-let star = "⭐"
-let fire = "🔥"
-let bell = "🔔"
-let bag = "💰"
-let trophy = "🏆"
+// Product A info
+let productA = {
+  emoji: "⭐",
+  revenue: 200,
+  commission: 50,
+};
 
-function addStar() {
-    salesIcons.textContent += star
-}
+// Product B info
+let productB = {
+  emoji: "🔥",
+  revenue: 300,
+  commission: 75,
+};
 
-function addFire() {
-    salesIcons.textContent += fire
-}
+let salesIconsArray = [];
+let achievementsArray = [];
+let bell = "🔔";
+let bag = "💰";
+let trophy = "🏆";
+let totalRev = 0;
+let totalCom = 0;
 
-starBtn.addEventListener('click', addStar)
-fireBtn.addEventListener('click', addFire)
+function addProduct(product) {
+    if (achievementsArray.length === 0) {
+      achievementsArray.push(bell);
+      achievementIcons.textContent = achievementsArray.join("");
+    }
+    salesIconsArray.push(product.emoji);
+    salesIcons.textContent = salesIconsArray.join("");
+    totalRev += product.revenue;
+    totalCom += product.commission;
+    revenueAmount.textContent = `$${totalRev}`;
+    commissionAmount.textContent = `$${totalCom}`;
+  
+    if (totalRev >= 2500 && achievementsArray.length < 2) {
+      achievementsArray.push(bag);
+      achievementIcons.textContent = achievementsArray.join("");
+    }
+  
+    if (salesIconsArray.length >= 15 && achievementsArray.length < 3) {
+      achievementsArray.push(trophy);
+      achievementIcons.textContent = achievementsArray.join("");
+    }
+    salesNum.textContent = salesIconsArray.length;
+    achievementsNum.textContent = achievementsArray.length;
+  }
+
+starBtn.addEventListener("click", function() {
+    addProduct(productA)
+});
+fireBtn.addEventListener("click", function() {
+    addProduct(productB)
+});
